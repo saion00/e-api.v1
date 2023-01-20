f605b6f829f6db3867094681cbd3d5df10e441dd
saion <saion.node@gmail.com>
2023-01-20 09:48:58 -0300

    second

--- routerCompras.js
+++ routerCompras.js
@@ -22,7 +22,7 @@ routerCompras.get("/", (req, res) => {
 });
 
 routerCompras.get("/:cid", async (req, res) => {
-    // console.log(req.params.pid);
+    // console.log(req.params.cid);
     var cartId = req.params.cid;
     var show = AllCart;
     // console.log(show);
@@ -66,42 +66,33 @@ routerCompras.post("/:cid/product/:pid", async (req, res) => {
     res.status(400).send("No existe el producto a cargar al carrito");
 });
 
-
-// routerCompras.get("/", (req, res) => {
-//     res.send(compras);
+// routerCompras.post("/:cid/product/:pid", async (req, res) => {
+//     var cid = req.params.cid;
+//     var pid = req.params.pid;
+//     var {quantity} = req.body;
+//     // console.log(cid);
+//     // console.log(pid);
+//     // console.log(quantity);
+//     // var show = allProducts;
+//     // // console.log(show);
+//     // var product = await show.find((e) => e.id === Number(pid));
+//     // var product = product || {};
+//     // var { "id": id } = product;
+//     // console.log(id);
+//     // console.log(product);
+//     // if (pid == id) {
+//         // res.send(product);
+//         if (cid.length && pid.length && quantity.length) { 
+//             console.log(cid);
+//             console.log(pid);
+//             console.log(quantity);
+//             // console.log(pid);
+//             cart.addProductToCart(cid, pid, quantity);
+//             return res.status(200).send("ok");
+//         }
+//     // }
+//     res.status(400).send("No existe el producto a cargar al carrito");
 // });
-
-
-// routerCompras.put("/:id", (req, res) => {
-//     compraId = compras.findIndex((c) => c.id === +req.params.id);
-//     compras[compraId] = {
-//         id: req.params.id,
-//         nombre: req.body.nombre,
-//     };
-//     res.send("producto actualizado!");
-// });
-
-
-// routerCompras.delete("/:id", (req, res) => {
-//     compras = compras.filter((c) => c.id !== +req.params.id);
-//     res.send("Producto eliminado");
-// });
-
-
-// routerCompras.post("/nueva", multer({ dest: "uploads/" }).single("foto"), function (req, res, next) {
-//     // req.file is the `avatar` file
-//     // req.body will hold the text fields, if there were any
-//     console.log(req.body);
-//     res.status(200).send("Compra agregada");
-// });
-
-
-// HOLA MUNDO
-// routerCompras.get("/", (req, res) => {
-//     res.send("Hola mundo!");
-// });
-
-// EXPORT ROUTER
 
 module.exports = {
     routerCompras,
